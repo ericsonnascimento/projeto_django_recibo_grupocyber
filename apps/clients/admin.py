0bc2473af2576a0467baa77545153bf7035886e5
ericsonnascimento <ericson@grupocyber.com.br>
2024-03-25 11:25:06 -0300

    update - realizada inclusão de código no admin.py do  apps/clients e apps/receipts no intuito de melhorar a experiência de uso no painel admin

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Client_Register)
+class ListingClients(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone',)
+    list_display_links = ('id', 'name',)
+    search_fields = ('id', 'client',)
+    list_filter = ('id', 'name', 'phone')
+    list_per_page = 10
+
+admin.site.register(models.Client_Register, ListingClients)
--- apps/receipts/admin.py
+++ apps/receipts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Receipts)
+class ListingReceipts(admin.ModelAdmin):
+    list_display = ('id', 'client', 'user', 'date_register',)
+    list_display_links = ('id', 'client',)
+    search_fields = ('id', 'client',)
+    list_filter = ('id', 'user', 'client',)
+    #list_editable = ('publicada',)
+    list_per_page = 10
+
+admin.site.register(models.Receipts, ListingReceipts)
